cf30ac20c9df4b72649667c7d58c58387ebdcdff
Ayan Biswas <149355413+Ayanbiswas123@users.noreply.github.com>
2024-03-14 11:06:04 +0530

    Add files via upload

---
+++ 01.Hello_World/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            int a, b, c;
+            Console.WriteLine("Enter The Value Of A & B = ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("The Sum of " + a + " And " + b + " is = " + c);
+            Console.ReadLine();
+
+
+        }
+    }
+}
