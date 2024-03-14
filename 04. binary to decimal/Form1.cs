77fd1c670e70ab0f94ca852b1dabe911c59c469b
Ayan Biswas <149355413+Ayanbiswas123@users.noreply.github.com>
2024-03-14 11:09:31 +0530

    Add files via upload

---
+++ 04. binary to decimal/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace binary_to_decimal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string binary = textBox1.Text;
+
+            int decimalValue = Convert.ToInt32(textBox1.Text,2);
+            String octalVaue = Convert.ToString(decimalValue, 8);
+
+            textBox2.Text = decimalValue.ToString();
+            textBox3.Text = octalVaue.ToString();
+        }
+    }
+}
