d47dbdcf57c1da3672c5691335a4e5a1228efd76
Ayan Biswas <149355413+Ayanbiswas123@users.noreply.github.com>
2024-03-14 11:06:49 +0530

    Add files via upload

---
+++ 02.Sum of two number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sum_of_two_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+            Console.WriteLine("Enter The Value Of A & B = ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("The Sum of " + a + " And " + b + " is = " + c);
+            Console.ReadLine();
+        }
+    }
+}
